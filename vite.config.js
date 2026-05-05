0fe0570ee7fc94b595f104f6cc20b9f397802fde
DDMAUApp <andrew.shih87@gmail.com>
2026-05-04 19:14:21 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const buildId = Date.now().toString(36)
+
 export default defineConfig({
   plugins: [react()],
   base: '/dd-mau-portal/',
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-${buildId}-[hash].js`,
+      }
+    }
   },
 })
